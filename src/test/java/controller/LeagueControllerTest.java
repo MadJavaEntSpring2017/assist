3c5466c49deb8d68a6798c2008f668a84b60c31c
Benjamin S Nisler <bnisler@gmail.com>
2017-02-12 10:01:18 -0600

    comment out a test for now

--- src/test/java/controller/LeagueControllerTest.java
+++ src/test/java/controller/LeagueControllerTest.java
@@ -1,21 +1,12 @@
 package controller;
 
 import com.bnisler.controller.LeagueController;
-import com.bnisler.entity.League;
-import com.bnisler.persist.LeagueDao;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.InjectMocks;
 import org.mockito.MockitoAnnotations;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-
-import java.util.List;
-
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 /**
  * Created by Ben on 2/4/2017.
@@ -25,9 +16,6 @@ public class LeagueControllerTest {
     @InjectMocks
     private LeagueController leagueController;
 
-//    @Autowired
-//    private LeagueDao leagueDao;
-
     private MockMvc mockMvc;
 
     @Before
@@ -36,9 +24,9 @@ public class LeagueControllerTest {
         mockMvc = MockMvcBuilders.standaloneSetup(leagueController).build();
     }
 
-//    @Test
-//    public void test() throws Exception {
+    @Test
+    public void test() throws Exception {
 //        mockMvc.perform(get("/leagues")).andDo(print());
-//    }
+    }
 
 }
