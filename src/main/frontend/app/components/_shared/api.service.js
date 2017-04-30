70a89d03aa039efebedd7e4b5684cea12859a4f7
Benjamin S Nisler <bnisler@gmail.com>
2017-04-30 16:13:25 -0500

    rfkr/api-service : create abstract service to make http method calls to server

---
+++ src/main/frontend/app/components/_shared/api.service.js
@@ -0,0 +1,68 @@
+(function () {
+    'use strict';
+    angular
+        .module('assist-web')
+        .factory('apiService', apiService);
+
+    apiService.$inject = ['$q', '$http'];
+
+    function apiService($q, $http) {
+        var BASE_URI = 'http://localhost:8080';
+        var REST_SERVICE_URI;
+
+        return {
+            get: get,
+            post: post,
+            put: put
+        };
+
+        function put(path, writeRequest) {
+            buildUri(path);
+            var deferred = $q.defer();
+            $http.put(REST_SERVICE_URI, writeRequest)
+                .then(
+                    function (results) {
+                        deferred.resolve(results.data);
+                    },
+                    function (error) {
+                        deferred.reject(error);
+                    }
+                );
+            return deferred.promise;
+        }
+
+        function post(path, writeRequest) {
+            buildUri(path);
+            var deferred = $q.defer();
+            $http.post(REST_SERVICE_URI, writeRequest)
+                .then(
+                    function (results) {
+                        deferred.resolve(results.data);
+                    },
+                    function (error) {
+                        deferred.reject(error);
+                    }
+                );
+            return deferred.promise;
+        }
+
+        function get(path) {
+            buildUri(path);
+            var deferred = $q.defer();
+            $http.get(REST_SERVICE_URI)
+                .then(
+                    function (results) {
+                        deferred.resolve(results.data);
+                    },
+                    function (error) {
+                        deferred.reject(error);
+                    }
+                );
+            return deferred.promise;
+        }
+
+        function buildUri(path) {
+            REST_SERVICE_URI = BASE_URI + path;
+        }
+    }
+})();
--- src/main/frontend/app/components/player/player.service.js
+++ src/main/frontend/app/components/player/player.service.js
@@ -4,87 +4,31 @@
         .module('assist-web')
         .factory('playerService', playerService);
 
-    playerService.$inject = ['$q', '$http'];
+    playerService.$inject = ['apiService'];
 
-    function playerService($q, $http) {
-        var REST_SERVICE_URI = 'http://localhost:8080/players/';
+    function playerService(apiService) {
 
         return {
             getAllPlayers: getAllPlayers,
-            getPlayer: getPlayer,
             getPlayerDetails: getPlayerDetails,
             createPlayer: createPlayer,
             updatePlayer: updatePlayer
         };
 
         function updatePlayer(playerId, writeRequest) {
-            var deferred = $q.defer();
-            $http.put(REST_SERVICE_URI + playerId, writeRequest)
-                .then(
-                    function (results) {
-                        deferred.resolve(results.data);
-                    },
-                    function (error) {
-                        deferred.reject('Error while updating player: ' + error);
-                    }
-                );
-            return deferred.promise;
+            return apiService.put('/players/' + playerId, writeRequest);
         }
 
         function createPlayer(writeRequest) {
-            var deferred = $q.defer();
-            $http.post(REST_SERVICE_URI, writeRequest)
-                .then(
-                    function (results) {
-                        deferred.resolve(results.data);
-                    },
-                    function (error) {
-                        deferred.reject('Error while creating player: ' + error);
-                    }
-                );
-            return deferred.promise;
-        }
-
-        function getPlayer(playerId) {
-            var deferred = $q.defer();
-            $http.get(REST_SERVICE_URI + playerId)
-                .then(
-                    function (results) {
-                        deferred.resolve(results.data);
-                    },
-                    function (error) {
-                        deferred.reject('Error while getting player: ' + error);
-                    }
-                );
-            return deferred.promise;
+            return apiService.post('/players', writeRequest);
         }
 
         function getPlayerDetails(playerId) {
-            var deferred = $q.defer();
-            $http.get(REST_SERVICE_URI + playerId + '/details')
-                .then(
-                    function (results) {
-                        deferred.resolve(results.data);
-                    },
-                    function (error) {
-                        deferred.reject('Error while getting player details: ' + error);
-                    }
-                );
-            return deferred.promise;
+            return apiService.get('/players/' + playerId + '/details');
         }
 
         function getAllPlayers() {
-            var deferred = $q.defer();
-            $http.get(REST_SERVICE_URI)
-                .then(
-                    function (results) {
-                        deferred.resolve(results.data);
-                    },
-                    function (error) {
-                        deferred.reject('Error while getting players: ' + error);
-                    }
-                );
-            return deferred.promise;
+            return apiService.get('/players');
         }
     }
 })();
