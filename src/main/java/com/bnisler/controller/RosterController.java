19a7ec715380afbffecf3f19b674dceb07f950ee
Benjamin S Nisler <bnisler@gmail.com>
2017-05-03 11:08:04 -0500

    feat/roster : controller method to create a roster

--- src/main/java/com/bnisler/controller/RosterController.java
+++ src/main/java/com/bnisler/controller/RosterController.java
@@ -5,6 +5,7 @@ import com.bnisler.service.commit.CommitService;
 import com.bnisler.service.commit.CommitWriteRequest;
 import com.bnisler.service.roster.RosterDetail;
 import com.bnisler.service.roster.RosterService;
+import com.bnisler.service.roster.RosterWriteRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -23,6 +24,12 @@ public class RosterController {
     @RequestMapping(value = "/rosters", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON)
     public List<RosterDetail> getAllRosters() {
         return rosterService.getAllRosters();
+    }
+
+    @RequestMapping(value = "/rosters", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON)
+    public RosterDetail createRoster(@RequestBody RosterWriteRequest writeRequest) {
+        Long rosterId = rosterService.createRoster(writeRequest);
+        return rosterService.getRosterDetails(rosterId);
     }
 
     @RequestMapping(value = "/rosters/{rosterId}/commits", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON)
